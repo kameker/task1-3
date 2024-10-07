f0fbf19ca205a24ff5018bbc0fffebf7008da73e
kameker <vsrnpra2@gmail.com>
2024-10-07 18:20:24 +0300

    5-ый 2-ая половина

--- src/vgu/dz/Main/Main.java
+++ src/vgu/dz/Main/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Mountain m = new Mountain(4);
+        Mountain m = new Mountain(99);
         m.drawMountain();
     }
 }
--- src/vgu/dz/fifth/Mountain.java
+++ src/vgu/dz/fifth/Mountain.java
@@ -7,17 +7,29 @@ public class Mountain {
         this.k = k;
     }
 
+
+//                /\
+//         /\    //\\    /\
+//    /\  //\\  ///\\\  //\\  /\
+// /\//\\///\\\////\\\\///\\\//\\/\
+
     public void drawMountain() {
         for (int i = 0; i < this.k; i++) {
             for (int j = 0; j < i + 1; j++) {
                 if (j == 0) {
                     drawSpace((this.k - i) * (this.k - i) - 1);
                     drawSlash(1);
+                    drawReSlash(1);
                 } else {
-                    drawReSlash(j);
                     drawSpace(2 * (this.k - i - 1));
                     drawSlash(j + 1);
+                    drawReSlash(j+1);
                 }
+            }
+            for (int j = i-1; j >= 0; j--) {
+                drawSpace(2 * (this.k - i - 1));
+                drawSlash(j+1);
+                drawReSlash(j+1);
             }
             System.out.println();
         }
