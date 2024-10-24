d62f64ff07ab2466cc2d46a388f5bd5d5fe4cbc2
kameker <vsrnpra2@gmail.com>
2024-10-24 19:48:38 +0300

    улучшено 6+

--- src/vgu/dz/sixth/Series.java
+++ src/vgu/dz/sixth/Series.java
@@ -4,21 +4,23 @@ package vgu.dz.sixth;
 import static vgu.dz.myfuncs.MyMath.supFactorial;
 
 public class Series {
+    public double x;
     public int n;
     public double e;
     public double ediv10;
     public double last = 0;
-    public double sumAllElements = 0;
-    public double sumElementsBiggerThanE = 0;
-    public double sumElementsBiggerThanEdiv10 = 0;
-    public Series(int n, double e){
+    private double sumAllElements = 0;
+    private double sumElementsBiggerThanE = 0;
+    private double sumElementsBiggerThanEdiv10 = 0;
+    public Series(int n, double e,double x){
         this.n = n;
+        this.x = x;
         this.e = e;
         this.ediv10 = this.e/10;
     }
     public void sumN(){
         for (int i = 0; i < this.n ; i++){
-            this.last = nElem(i,this.last,0);
+            this.last = nElem(i,this.last,this.x);
             this.sumAllElements += this.last;
             if (this.last > this.e){
                 this.sumElementsBiggerThanE += this.last;
@@ -28,7 +30,16 @@ public class Series {
             }
         }
     }
-    public double nElem(int n, double Anm1, int x){
-        return Anm1 + Math.pow(-1,n)*supFactorial(n,1)/supFactorial(n,2);
+    public double nElem(int n, double Anm1, double x){
+        return Anm1 + x*Math.pow(-1,n)*supFactorial(n,1)/supFactorial(n,2);
+    }
+    public double getSumAllElements(){
+        return this.sumAllElements;
+    }
+    public double getSumElementsBiggerThanE(){
+        return this.sumElementsBiggerThanE;
+    }
+    public double getSumElementsBiggerThanEdiv10(){
+        return this.sumElementsBiggerThanEdiv10;
     }
 }
